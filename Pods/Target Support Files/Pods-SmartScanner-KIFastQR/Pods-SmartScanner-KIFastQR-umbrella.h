df75c597f40451456d4fbbc09e67cf37c88506e0
munenori <flashingwind@gmail.com>
2015-07-17 23:51:48 +0900

    KIFastQR導入。

---
+++ Pods/Target Support Files/Pods-SmartScanner-KIFastQR/Pods-SmartScanner-KIFastQR-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "KIFastQRCaptureView.h"
+
+FOUNDATION_EXPORT double KIFastQRVersionNumber;
+FOUNDATION_EXPORT const unsigned char KIFastQRVersionString[];
+
---
+++ Pods/Target Support Files/Pods-SmartScanner/Pods-SmartScanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartScanner : NSObject
+@end
+@implementation PodsDummy_Pods_SmartScanner
+@end
